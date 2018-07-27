875b15bffda3a6777c49b8216deadc1e02850cc6
Pranav Vashi <neobuddy89@gmail.com>
2018-07-28 05:13:16 +0530

    angler: Boost for 800ms
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- init.angler.power.sh
+++ init.angler.power.sh
@@ -89,7 +89,7 @@ write /sys/module/msm_performance/parameters/cpu_max_freq "4:4294967295 5:429496
 
 # input boost configuration
 write /sys/module/cpu_boost/parameters/input_boost_freq "0:1344000"
-write /sys/module/cpu_boost/parameters/input_boost_ms 40
+write /sys/module/cpu_boost/parameters/input_boost_ms 800
 
 # Setting B.L scheduler parameters
 write /proc/sys/kernel/sched_migration_fixup 1
