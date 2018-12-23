099671ef99dfb52fef672e8d8dbeb4bcfa9df5d1
Pranav Vashi <neobuddy89@gmail.com>
2018-12-23 18:51:15 +0530

    angler: Add manifest for bluetooth
    Change-Id: I6b29d1806dcb0bcbd7a1f347883ee925d68ce0a6
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -318,7 +318,8 @@ PRODUCT_COPY_FILES += \
 # Bluetooth HAL
 PRODUCT_PACKAGES += \
     libbt-vendor \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
 
 # NFC
 PRODUCT_PACKAGES += \
