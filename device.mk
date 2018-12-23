8488396fcc207da20415cbd62f8fad83aa5f47bd
Pranav Vashi <neobuddy89@gmail.com>
2018-12-23 18:24:33 +0530

    angler: Add manifest for graphics composer
    Change-Id: Ic4d13f48c66f3db3cd198699178b49531ce3c45e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -251,7 +251,8 @@ PRODUCT_PACKAGES += \
     gps.msm8994
 
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.1-impl
+    android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service
 
 # Sensor & activity_recognition HAL
 TARGET_USES_NANOHUB_SENSORHAL := true
