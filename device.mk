e24d1a1e15692877cbe01c118293694c56d38dcf
Pranav Vashi <neobuddy89@gmail.com>
2018-12-23 18:52:58 +0530

    angler: Add manifest for audio
    Change-Id: I9e6fb2a73662433195308c5e7c859f12b5ba457e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -234,8 +234,11 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.soundtrigger@2.0-impl
+    android.hardware.audio.effect@2.0-service \
+    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.0-service
 
 #CAMERA
 PRODUCT_PACKAGES += \
