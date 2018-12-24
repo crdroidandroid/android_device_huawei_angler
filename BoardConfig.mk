8f1e2771dad278c277765d17d3f86b87809501bc
Pranav Vashi <neobuddy89@gmail.com>
2018-12-24 19:40:37 +0530

    angler: Enable dexpreopt for boot img and system server only
    Change-Id: Idf348e9c7c448f0cc746719d3aee9019e700bb94
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,8 +108,8 @@ BOARD_CHARGER_ENABLE_SUSPEND := true
 ifeq ($(HOST_OS),linux)
   ifneq ($(TARGET_BUILD_VARIANT),eng)
     ifeq ($(WITH_DEXPREOPT),)
-      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
       WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
     endif
   endif
 endif
