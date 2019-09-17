9182493cd3c245103f836eaef046d454b6dcdd42
J0SH1X <aljoshua.hell@gmail.com>
2019-09-17 18:45:20 +0200

    angler: fix build fingerprint missmatch

--- statix_angler.mk
+++ statix_angler.mk
@@ -39,4 +39,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
     PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM3.171019.014 4503998 release-keys"
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/angler/angler:8.1.0/OPM3.171019.014/4503998:user/release-keys
+
 BUILD_FINGERPRINT := google/angler/angler:8.1.0/OPM3.171019.014/4503998:user/release-keys
