1d3818708e7a90c4e80396117e890fbb66d3d4f2
PixelBoot <bootingpixels@gmail.com>
2019-12-02 12:09:35 -0800

    angler: build trust hal

--- device.mk
+++ device.mk
@@ -349,6 +349,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.health@2.0-service.angler
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 #USB HAL
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
