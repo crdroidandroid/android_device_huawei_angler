9d277f452a4a2409003ce58f4d883cf04c5b1c6b
anayw2001 <awadhera@berkeley.edu>
2020-04-10 11:38:06 +0200

    angler: prebuilt keystore

--- device.mk
+++ device.mk
@@ -592,8 +592,7 @@ PRODUCT_COPY_FILES += \
 
 # services for encryption
 PRODUCT_PACKAGES += \
-    gatekeeper.msm8994 \
-    keystore.msm8994
+    gatekeeper.msm8994
 
 # general support
 PRODUCT_PACKAGES += \
