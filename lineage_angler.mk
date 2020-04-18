7d58a2a39ee9805112c0f59f0c7f648d5881b69f
Pranav Vashi <neobuddy89@gmail.com>
2020-04-18 12:29:52 +0530

    angler: Remove lineage updater URI
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- lineage_angler.mk
+++ lineage_angler.mk
@@ -37,11 +37,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
     PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM7.181205.001 5080180 release-keys"
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.fingerprint=google/angler/angler:8.1.0/OPM7.181205.001/5080180:user/release-keys
-
 BUILD_FINGERPRINT := google/angler/angler:8.1.0/OPM7.181205.001/5080180:user/release-keys
-
-# Lineage Updater
-PRODUCT_PROPERTY_OVERRIDES += \
-    lineage.updater.uri=https://raw.githubusercontent.com/PixelBoot/Lineage-OTA/lineage-17.1/angler.json
